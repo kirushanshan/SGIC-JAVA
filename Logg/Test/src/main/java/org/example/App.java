afd7737eab9a5a35658838a1ea66ee888d8744a2
kirushanshan <kirushan93@gmail.com>
2022-09-12 08:46:22 +0530

    Loging and java 8 features

---
+++ Logg/Test/src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+package org.example;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    private static Logger logger = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        logger.trace("This is trace msg");
+        logger.info("This is information");
+        logger.error("this is an error message");
+        logger.warn("this is a warning message");
+        logger.fatal("This is a fatal msg");
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maven/src/main/java/LogDemoSG/Log4jDemo.java
@@ -0,0 +1,10 @@
+package LogDemoSG;
+
+public class Log4jDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
