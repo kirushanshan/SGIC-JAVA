f1b145d2d6ad1a54b580ab85650ad6b7a66da98b
krish <kirushan93@gmail.com>
2022-09-21 12:42:51 +0530

    servelet

---
+++ JSP and Servelet/Student/src/main/java/Student.java
@@ -0,0 +1,49 @@
+
+public class Student {
+	int id;
+	String name;
+	int age;
+	String address;
+	int grade;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+}
